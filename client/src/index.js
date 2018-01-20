4e0d3eda9d85bc5b0205a5622577b6fff0cd4766
CORP\saidm <saidm@surveymonkey.com>
2018-01-20 01:39:59 -0500

    initial project seed

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#bidorboo-app")
+);
+registerServiceWorker();
