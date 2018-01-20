af18759e3c8471ad192c4975a93af106d55fd098
CORP\saidm <saidm@surveymonkey.com>
2018-01-20 18:33:34 -0500

    add configs and passportjs

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+const passport = require ('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 app.get("/", (req, res) => {
   res.send({
     hi: "hey there"
   });
 });
+// client id
+// client secret
+passport.use(new GoogleStrategy())
 
-
-console.log('I made it ssssssssssssssss' + process.env.PORT)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
