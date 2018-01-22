92cf62215371eb28b346718170762d099b2e6232
CORP\saidm <saidm@surveymonkey.com>
2018-01-21 21:24:13 -0500

    added react toolbox + example navbar + notes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
-
 import logo from "../assets/logo.svg";
 import "./App.scss";
+import AppBar from "react-toolbox/lib/app_bar/AppBar";
+import Navigation from "react-toolbox/lib/navigation/Navigation";
+import Link from "react-toolbox/lib/link/Link";
+
+const GithubIcon = () => <img alt="logo" src={logo}/>;
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <nav>
-          <div className="nav-wrapper">
-            <a href="#" className="brand-logo">
-              BidOrBoo
-            </a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li>
-                <a href="sass.html">inbox</a>
-              </li>
-              <li>
-                <a href="badges.html">stuff</a>
-              </li>
-              <li>
-                <a href="collapsible.html">username</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
+      <AppBar title="BidOrBoo" leftIcon="menu" rightIcon={<GithubIcon />}>
+        <Navigation type="horizontal">
+          <Link href="http://" label="Inbox" icon="inbox" />
+          <Link href="http://" active label="Profile" icon="person" />
+        </Navigation>
+      </AppBar>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+
+
+//offline mode support
+import registerServiceWorker from "./registerServiceWorker";
+
+//redux
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+
+//react toolbox
+import './assets/react-toolbox/theme.scss';
+import theme from './assets/react-toolbox/theme.js'
+import ThemeProvider from "react-toolbox/lib/ThemeProvider";
+
+//global styles
 import "./index.scss";
 import 'materialize-css/dist/css/materialize.min.css';
 
-import registerServiceWorker from "./registerServiceWorker";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-
 import App from "./containers/App";
-import reducers from "./reducers";
+
 
 const store = createStore(reducers, {}, applyMiddleware());
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("bidorboo-app")
 );
