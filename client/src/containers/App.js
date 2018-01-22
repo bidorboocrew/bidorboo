636aa31b82ae993e4a9e5f9e3336ebc157d558dd
CORP\saidm <saidm@surveymonkey.com>
2018-01-21 21:24:13 -0500

    added react toolbox + example navbar + notes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import logo from "../assets/logo.svg";
+import "./App.scss";
+import AppBar from "react-toolbox/lib/app_bar/AppBar";
+import Navigation from "react-toolbox/lib/navigation/Navigation";
+import Link from "react-toolbox/lib/link/Link";
 
+
+const GithubIcon = () => <img alt="logo" src={logo}/>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppBar title="BidOrBoo" leftIcon="menu" rightIcon={<GithubIcon />}>
+        <Navigation type="horizontal">
+          <Link href="http://" label="Inbox" icon="inbox" />
+          <Link href="http://" active label="Profile" icon="person" />
+        </Navigation>
+      </AppBar>
     );
   }
 }
