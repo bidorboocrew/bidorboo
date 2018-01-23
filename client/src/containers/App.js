f72e854a7209ec64c41a9ff9a61d49b2361f3d40
CORP\saidm <saidm@surveymonkey.com>
2018-01-22 22:47:02 -0500

    added material css for side help with styles

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
-import './App.scss';
+import React, { Component } from "react";
 
+import logo from "../assets/logo.svg";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">
+              BidOrBoo
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">inbox</a>
+              </li>
+              <li>
+                <a href="badges.html">stuff</a>
+              </li>
+              <li>
+                <a href="collapsible.html">username</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
+import 'materialize-css/dist/css/materialize.min.css';
+
 import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
