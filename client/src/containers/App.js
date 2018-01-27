2a68937d010613b29f73de9f0fbcb0e93e3151a5
CORP\saidm <saidm@surveymonkey.com>
2018-01-27 01:13:27 -0500

    basic start

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
-import logo from "../assets/logo.svg";
-import "./App.scss";
-import AppBar from "react-toolbox/lib/app_bar/AppBar";
-import Navigation from "react-toolbox/lib/navigation/Navigation";
-import Link from "react-toolbox/lib/link/Link";
-
-const GithubIcon = () => <img alt="logo" src={logo}/>;
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <AppBar title="BidOrBoo" leftIcon="menu" rightIcon={<GithubIcon />}>
-        <Navigation type="horizontal">
-          <Link href="http://" label="Inbox" icon="inbox" />
-          <Link href="http://" active label="Profile" icon="person" />
-        </Navigation>
-      </AppBar>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-
 //offline mode support
 import registerServiceWorker from "./registerServiceWorker";
 
@@ -11,23 +9,16 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 
-//react toolbox
-import './assets/react-toolbox/theme.scss';
-import theme from './assets/react-toolbox/theme.js'
-import ThemeProvider from "react-toolbox/lib/ThemeProvider";
 
 //global styles
 import "./index.scss";
 
 import App from "./containers/App";
 
-
 const store = createStore(reducers, {}, applyMiddleware());
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </Provider>,
   document.getElementById("bidorboo-app")
 );
