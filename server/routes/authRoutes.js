4ed094d098cadb3731c53b77e107edfa48adb0ea
CORP\saidm <saidm@surveymonkey.com>
2018-01-27 15:21:02 -0500

    added logout + currentuser APIS

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-module.exports = (app) => {
+module.exports = app => {
   app.get('/', (req, res) => {
     res.send({
       hi: 'hey there'
@@ -13,4 +13,13 @@ module.exports = (app) => {
     })
   );
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get('/api/logout', (req, res)=>{
+    req.logout();
+    res.send(req.user);
+  })
 };
