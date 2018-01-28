fdf9cce1923bc720ef6c42a8bdd14e5a9ee88f0b
CORP\saidm <saidm@surveymonkey.com>
2018-01-28 02:35:09 -0500

    Added Material UI for fast development + updated npm packages + added app bar

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,54 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 //offline mode support
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
 //redux
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reducers from "./reducers";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
+//materialize css
+import 'typeface-roboto';
+
+import Reboot from 'material-ui/Reboot'; //this will do the normalization to avoid inconsistency accross browsers
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
 //global styles
-import "./index.css";
+import './index.css';
 
-import App from "./containers/App";
+import App from './containers/App';
 
+//override some theme vars
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffffff',
+      main: '#fafafa',
+      dark: '#c7c7c7',
+      contrastText: '#000000'
+    },
+    secondary: {
+      light: '#87ffff',
+      main: '#4bdbdb',
+      dark: '#00a9aa',
+      contrastText: '#2b2b2b'
+    }
+  }
+});
+
+//create our beautiful redux store
 const store = createStore(reducers, {}, applyMiddleware());
+
 ReactDOM.render(
   <Provider store={store}>
+    <Reboot>
+      <MuiThemeProvider theme={theme}>
       <App />
+      </MuiThemeProvider>
+    </Reboot>
   </Provider>,
-  document.getElementById("BidOrBoo-app")
+  document.getElementById('BidOrBoo-app')
 );
 registerServiceWorker();
