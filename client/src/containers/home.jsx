2391753adae2cdb09a0c65249b4cd77e4b992eef
CORP\saidm <saidm@surveymonkey.com>
2018-01-29 13:16:15 -0500

    changed names / added more entry points

---
+++ client/src/containers/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles/content.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div id="bob-root-content" className="contentWrapper">
+        <div className="__introImage"> </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
