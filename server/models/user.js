3851680b1e2633215374528a62e7549044a35eae
Yacoub abdulla <yacoub.abdulla89@gmail.com>
2018-01-28 20:01:53 -0500

    -added facebook auth. need testing

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String,
+  Id: String,
   name: String
 });
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,6 +6,7 @@ module.exports = app => {
     });
   });
 
+  //google routes
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -14,12 +15,24 @@ module.exports = app => {
   );
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  //Facebook routes
+  app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook', { session: false })
+  );
+  app.get(
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', { session: false })
+  );
+
+  // shared
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 
-  app.get('/api/logout', (req, res)=>{
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
-  })
+  });
+
 };
