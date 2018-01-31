21068758e3f071211aed00c6f3dcf505ae2cb40f
CORP\saidm <saidm@surveymonkey.com>
2018-01-31 03:13:16 -0500

    navbar

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -13,22 +13,20 @@ class Header extends React.Component {
           <div className="__logo">
             <i className="material-icons">menu</i>
           </div>
-          <div className="__name hide-on-small-and-down">
-            BidOrBoo
-          </div>
+          <div className="__name hide-on-small-and-down">BidOrBoo</div>
           <div className="__search">
-          <div className="search-wrapper">
-            {/* <i class="material-icons">search</i> */}
-            <input className="app-bar-main-search" />
+            <div className="search-wrapper">
+              {/* <i class="material-icons">search</i> */}
+              <input className="app-bar-main-search" />
 
-            <div className="search-results"></div>
-          </div>
+              <div className="search-results" />
+            </div>
           </div>
           <div className="__button_FC hide-on-small-and-down">
             <div className="__buttons">
-            my button container
+              <a className="btn flat medium hover-effect">Signup</a>
+              <a className="btn flat medium hover-effect">Login</a>
             </div>
-
           </div>
         </div>
       </nav>
