6ce8bad32382d61f96ab9d7fa7cc02d90a411105
CORP\saidm <saidm@surveymonkey.com>
2018-01-31 02:32:26 -0500

    appbar responsive

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import './styles/header.css';
 
@@ -8,7 +9,28 @@ class Header extends React.Component {
   render() {
     return (
       <nav>
+        <div className="applicationBar-FC">
+          <div className="__logo">
+            <i className="material-icons">menu</i>
+          </div>
+          <div className="__name hide-on-small-and-down">
+            BidOrBoo
+          </div>
+          <div className="__search">
+          <div className="search-wrapper">
+            {/* <i class="material-icons">search</i> */}
+            <input className="app-bar-main-search" />
 
+            <div className="search-results"></div>
+          </div>
+          </div>
+          <div className="__button_FC hide-on-small-and-down">
+            <div className="__buttons">
+            my button container
+            </div>
+
+          </div>
+        </div>
       </nav>
     );
   }
