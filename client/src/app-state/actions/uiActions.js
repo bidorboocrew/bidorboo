e8bab3b07b12c1dd34b0e3cb1d9f7dd43e93b7c9
CORP\saidm <saidm@surveymonkey.com>
2018-02-07 00:39:38 -0500

    minor style fix

--- client/src/app-state/actions/uiActions.js
+++ client/src/app-state/actions/uiActions.js
@@ -1,13 +1,8 @@
 import * as A from '../actionTypes';
 
-export const a_toggleSideNav = (isopen) =>
-   {
-    type: !isopen
-      ? A.UI_ACTIONS.OPEN_SIDENAV
-      : A.UI_ACTIONS.CLOSE_SIDENAV,
-    payload: {
-      isSideBarOpen: !isopen
-    }
+export const a_toggleSideNav = isopen => ({
+  type: !isopen ? A.UI_ACTIONS.OPEN_SIDENAV : A.UI_ACTIONS.CLOSE_SIDENAV,
+  payload: {
+    isSideBarOpen: !isopen
   }
-
-
+});
