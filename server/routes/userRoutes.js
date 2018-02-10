ca656c53b54084735984821113c03586066bbdb5
CORP\saidm <saidm@surveymonkey.com>
2018-02-10 14:30:45 -0500

    add require login middle ware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,9 +2,7 @@ const userDataAccess = require('../data-access/userDataAccess');
 const ROUTES = require('./route_constants');
 
 module.exports = app => {
-  const x = ROUTES.API.GET_CURRENTUSER;
     app.get(ROUTES.API.GET_CURRENTUSER, (req, res) => {
-      debugger;
       res.send(req.user);
     });
 
