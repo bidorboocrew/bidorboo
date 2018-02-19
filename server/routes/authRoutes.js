15bc96627f58ebbe232dff1c055bae89211722f4
saidmadi <bidorboocrew@gmailcom>
2018-02-19 15:01:55 -0500

    removed redundant google auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,35 +1,8 @@
 const passport = require('passport');
 const ROUTES = require('../route_constants');
-const keys = require('../config/keys');
-
-//ANDROID only
-const GoogleAuth = require('google-auth-library');
-const auth = new GoogleAuth;
-const client = new auth.OAuth2(keys.googleClientID, '', '');
-
-//ANDROID only
-
 
 module.exports = app => {
-app.get('/auth/verifyuser'),
-  (req, res) => {
-
-    client.verifyIdToken(
-      token,
-      CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
-      (e, login) => {
-        var payload = login.getPayload();
-        var userid = payload['sub'];
-        // If request specified a G Suite domain:
-        //var domain = payload['hd'];
-      });
-    return user
-  }
-}
-
-  // google routes
+  //google routes
   app.get(
     ROUTES.AUTH.GOOGLE,
     passport.authenticate('google', {
@@ -58,10 +31,4 @@ app.get('/auth/verifyuser'),
     req.logout();
     res.redirect(ROUTES.ENTRY);
   });
-
-  //-----------------------------------------------
-
-
-
-
 };
