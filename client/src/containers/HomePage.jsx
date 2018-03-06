7d774909bf55fab4a95366e6f0ece0481015d4be
saidmadi <bidorboocrew@gmailcom>
2018-03-05 22:01:48 -0500

    fix the header section

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -4,11 +4,25 @@ import './styles/home.css';
 class HomePage extends React.Component {
   render() {
     return (
-      <section className="section">
-      <div style={{background : 'purple'}}  className="container" id="bdb-home-content" >
+      <div>
+        <section className="hero">
+          <div style={{background: '#363636'}} className="hero-body">
+            <div className="container">
+              <h1 style={{color:'white'}}  className="title">BidOrBoo</h1>
+              <h2 style={{color:'white'}}  className="subtitle">Get tasks done for the price you want.<br/> Earn money doing what you love</h2>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <div
+            style={{ background: 'purple' }}
+            className="container"
+            id="bdb-home-content"
+          >
             HomePage
+          </div>
+        </section>
       </div>
-      </section>
     );
   }
 }
