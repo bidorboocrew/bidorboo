ceca0f4e8d08cc852d5d31e45b76f10549aef568
bidorboocrew <35621639+bidorboocrew@users.noreply.github.com>
2018-03-13 21:49:13 -0400

    google maps working 101 (#16)

--- client/src/containers/BidderContainer.jsx
+++ client/src/containers/BidderContainer.jsx
@@ -1,17 +1,83 @@
 import React from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
+import { compose, withProps } from 'recompose';
+// https://tomchentw.github.io/react-google-maps/#infobox
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyD0th06BSi2RQMJH8_kCsSdBfMRW4MbrjU&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 45.4215, lng: -75.6972 }}>
+    {props.isMarkerShown && (
+      <Marker
+        position={{lat: 45.4215, lng: -75.6972}}
+        onClick={props.onMarkerClick}
+      >
+        {props.showInfo && (
+          <InfoBox
+            onCloseClick={props.onToggleOpen}
+            options={{ closeBoxURL: ``, enableEventPropagation: true }}
+          >
+            <div
+              style={{
+                backgroundColor: `white`,
+                // opacity: 0.8,
+                padding: `12px`
+              }}
+            >
+              <div style={{ fontSize: `16px`, fontColor: `#4a4a4a` }}>
+                Hello, Yacoub! isn't this awesome
+              </div>
+            </div>
+          </InfoBox>
+        )}
+      </Marker>
+    )}
+  </GoogleMap>
+));
 
 class BidderContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.toggleShow = ()=>{
+      this.setState({show: !this.state.show});
+    }
+  }
   render() {
     return (
-      <section className="section">
-        <div
-          style={{ background: 'green' }}
-          className="container"
-          id="bdb-bidder-content"
-        >
-          home Bidder Content
-        </div>
-      </section>
+      <div id="bdb-bidder-content">
+        <section className="section">
+          <div className="container">
+            <div id="job-types">
+              <div className="bdb-section-title">The Map View</div>
+            </div>
+            <div className="bdb-section-body" id="existing-jobs">
+              <div className="columns">
+                <div className="column">
+                  <MyMapComponent
+                    showInfo={this.state.show}
+                    isMarkerShown={true}
+                    onMarkerClick={this.toggleShow}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
   }
 }
--- client/src/containers/ProposerContainer.jsx
+++ client/src/containers/ProposerContainer.jsx
@@ -1,19 +1,67 @@
 import React from 'react';
+import { bidorbooDefaultTasks } from '../constants/bidorbooDefaultTasks';
 
 class ProposerContainer extends React.Component {
+
   render() {
+    const defaultTasks = bidorbooDefaultTasks.map(defaultTask => (
+      <BidOrBooDefaultTask key={defaultTask.id} {...defaultTask} />
+    ));
     return (
-      <section className="section">
-        <div
-          style={{ background: 'blue' }}
-          className="container"
-          id="bdb-proposer-content"
-        >
-          Proposer Content
-        </div>
-      </section>
+      <div id="bdb-proposer-content">
+        <section className="section">
+          <div className="container">
+            <div id="job-types">
+              <div className="bdb-section-title">Post a Job</div>
+            </div>
+            <div className="bdb-section-body" id="existing-jobs">
+              <div className="columns">{defaultTasks}</div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
   }
 }
 
 export default ProposerContainer;
+
+class BidOrBooDefaultTask extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: false
+    };
+    this.alterHoverState = val => {
+      this.setState({ isHover: val });
+    };
+  }
+
+  render() {
+    const { title, subtitle, description, imageUrl } = this.props;
+
+    return (
+      <div className="column bdbCardComponent">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-3by4">
+              <img src={imageUrl} alt={subtitle} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <h1 className="bdb-section-title">{title}</h1>
+            <div className="content">
+              <div className="descriptoin-section">{description}</div>
+              <div style={{ textAlign: 'center', marginTop: 8 }}>
+                <a className="button is-primary" disabled>
+                  {/* <i className="far fa-edit" style={{ fontSize: 12 }} /> */}
+                  <span style={{ marginLeft: 4 }}>{`Request ${title}`} </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
