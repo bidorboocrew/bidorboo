c4b988a9f8ce4c2b3e9f51bf7b08d1485eababee
saidmadi <bidorboocrew@gmailcom>
2018-04-16 21:40:35 -0400

    prevent react unless login

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -144,7 +144,7 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
       {...rest}
       render={props => {
         const { isLoggedIn } = { ...rest };
-        return true ? (
+        return isLoggedIn ? (
           <Component {...props} />
         ) : (
           <Redirect to={ROUTES.FRONTENDROUTES.HOME} />
