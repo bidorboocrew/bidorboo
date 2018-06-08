128b1a77f5620b0fbb598c4eb96c4e1bf9f112be
saidmadi <bidorboocrew@gmailcom>
2018-06-08 00:25:57 -0400

    added map to the main page

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -9,6 +9,9 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { showLoginDialog } from '../app-state/actions/uiActions';
 import { switchRoute } from '../app-state/actions/routerActions';
+import BidderMapSection from '../components/BidderMapSection';
+import { selectJobToBidOn } from '../app-state/actions/bidsActions';
+import { getAllPostedJobs } from '../app-state/actions/jobActions';
 
 class HomePage extends React.Component {
   static propTypes = {
@@ -17,15 +20,26 @@ class HomePage extends React.Component {
 
   componentDidMount() {
     window.scrollTo(0, 0);
-    const { s_resolvedLogin, a_showLoginDialog, match } = this.props;
+    const {
+      a_getAllPostedJobs,
+      s_resolvedLogin,
+      a_showLoginDialog,
+      match
+    } = this.props;
     const shouldShowLoginDialog = match.params.showLoginDialog;
     if (s_resolvedLogin && shouldShowLoginDialog) {
       a_showLoginDialog(true);
     }
+    a_getAllPostedJobs();
   }
 
   render() {
-    const { a_switchRoute } = this.props;
+    const {
+      a_switchRoute,
+      a_selectJobToBidOn,
+      s_mapCenterPoint,
+      s_allThePostedJobsList
+    } = this.props;
     return (
       <div id="bdb-home-content">
         <section className="hero is-small is-dark">
@@ -47,21 +61,7 @@ class HomePage extends React.Component {
         <section className="mainSectionContainer">
           <Fade left>
             <div className="container">
-              {/* <section className="hero is-dark is-small is-marginless is-hidden-mobile">
-                <div
-                  style={{ backgroundColor: '#e98969' }}
-                  className="hero-body"
-                >
-                  <div className="container">
-                    <h1 className="title">Post Jobs</h1>
-                    <h2 className="subtitle">
-                      Get the work done for the best price possible
-                    </h2>
-                  </div>
-                </div>
-              </section> */}
               <div className="card-header-title">Start posting jobs</div>
-
               <div className="container">
                 <div
                   // style={{alignItems:'flex-end'}}
@@ -75,49 +75,38 @@ class HomePage extends React.Component {
         </section>
 
         <section className="mainSectionContainer">
-          <Fade right>
-            <div className="container">
-              {/* <section
-                style={{ margin: '0.25rem' }}
-                className="hero is-dark is-small is-marginless is-hidden-mobile"
-              >
-                <div
-                  style={{ backgroundColor: '#c786f8' }}
-                  className="hero-body"
-                >
-                  <div className="container">
-                    <h1 className="title">Bid on Jobs</h1>
-                    <h2 className="subtitle">
-                      Start Earning money by doing things you are good at.
-                    </h2>
-                  </div>
-                </div>
-              </section> */}
-              <div className="card-header-title">Start Bidding on jobs</div>
+          <div className="container">
+            <div className="card-header-title">Start Bidding on jobs</div>
 
-              <div style={{ marginTop: '0.45rem' }}>
-                <div className="columns">
-                  <div className="column has-text-centered">
-                    <p>this is under development</p>
-                  </div>
-                </div>
-              </div>
+            <div style={{ marginTop: '0.45rem' }}>
+              <BidderMapSection
+                selectJobToBidOn={a_selectJobToBidOn}
+                mapCenterPoint={s_mapCenterPoint}
+                jobsList={s_allThePostedJobsList}
+              />
             </div>
-          </Fade>
+          </div>
         </section>
       </div>
     );
   }
 }
-const mapStateToProps = ({ authReducer }) => {
+const mapStateToProps = ({ authReducer, jobsReducer }) => {
   return {
-    s_resolvedLogin: authReducer.resolvedLogin
+    s_resolvedLogin: authReducer.resolvedLogin,
+    s_mapCenterPoint: jobsReducer.mapCenterPoint,
+    s_allThePostedJobsList: jobsReducer.allThePostedJobsList
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
+    a_getAllPostedJobs: bindActionCreators(getAllPostedJobs, dispatch),
     a_showLoginDialog: bindActionCreators(showLoginDialog, dispatch),
-    a_switchRoute: bindActionCreators(switchRoute, dispatch)
+    a_switchRoute: bindActionCreators(switchRoute, dispatch),
+    a_selectJobToBidOn: bindActionCreators(selectJobToBidOn, dispatch)
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomePage);
