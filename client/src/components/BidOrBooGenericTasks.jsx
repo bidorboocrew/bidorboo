99a0004865b0cff560b1192e1e30bf1b5f5999da
saidmadi <bidorboocrew@gmailcom>
2018-11-07 22:31:00 -0500

    new generic card design

--- client/src/components/BidOrBooGenericTasks.jsx
+++ client/src/components/BidOrBooGenericTasks.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
-
+import { randomColor } from 'randomcolor';
 import { templatesRepo } from '../constants/bidOrBooTaskRepo';
 import PropTypes from 'prop-types';
 import * as ROUTES from '../constants/frontend-route-consts';
@@ -17,6 +16,12 @@ class BidOrBooGenericTasks extends React.Component {
     const genericTasks = Object.keys(templatesRepo).map((key) => {
       const defaultTask = templatesRepo[key];
       const { title, subtitle, description, imageUrl, id } = defaultTask;
+
+      const bgcolor = `${randomColor({
+        luminosity: 'dark',
+        format: 'rgba',
+        alpha: 0.9,
+      })}`;
       return (
         <div key={id} className="column fade-in is-one-third">
           <div
@@ -29,18 +34,44 @@ class BidOrBooGenericTasks extends React.Component {
                 switchRoute(`${ROUTES.CLIENT.PROPOSER.createjob}/${id}`);
               }
             }}
-            className="card"
+            className="card bdb-genericJobCard"
           >
+            <header style={{ borderBottom: '1px solid #eee' }}>
+              <p
+                style={{
+                  border: 'none',
+                  padding: 28,
+                  backgroundColor: bgcolor,
+                  borderRadius: 0,
+                  color: 'white',
+                }}
+                className="button is-fullwidth has-text-centered is-capitalized has-text-weight-bold is-size-4"
+              >
+                {title}
+              </p>
+            </header>
             <div className="card-image is-clipped">
               <figure className="image is-3by1">
                 <img src={imageUrl} alt={subtitle} />
               </figure>
             </div>
             <div className="card-content">
-              <h1 className="title">
-                <a style={{ borderRadius: 0 }} className="button is-primary is-large is-fullwidth">
-                  <span>{title}</span>
-                </a>
+              <h1>
+                <div className="HorizontalAligner-center">
+                  <a
+                    style={{
+                      fontSize: '1.8rem',
+                      border: 'none',
+                      marginTop: '-55px',
+                      color: 'white',
+                      borderRadius: '100%',
+                      backgroundColor: bgcolor,
+                    }}
+                    className="button is-large bdb-AddJobButton"
+                  >
+                    <span>+</span>
+                  </a>
+                </div>
               </h1>
               <div className="content">
                 <div className="descriptoin-section">{description}</div>
