01aa3ba440836dbc961aaef031ddf538548d148b
saidmadi <bidorboocrew@gmailcom>
2018-11-22 00:22:17 -0500

    cleanup

--- client/src/app-state/actions/authActions.js
+++ client/src/app-state/actions/authActions.js
@@ -21,8 +21,8 @@ export const getCurrentUser = () => (dispatch) =>
 
           // initialize uploader widget on window obj
 
-          initializeProfileImgUploaderWidget(resp);
-          initializeJobImgUploaderWidget(resp);
+          // initializeProfileImgUploaderWidget(resp);
+          // initializeJobImgUploaderWidget(resp);
         } else {
           //rediret user to sign up page
           switchRoute(ROUTES.CLIENT.ENTRY);
@@ -194,146 +194,146 @@ const initializeProfileImgUploaderWidget = (resp) => {
       };
 };
 
-const initializeJobImgUploaderWidget = (resp) => {
-  window.BidOrBoo = window.BidOrBoo ? window.BidOrBoo : {};
-  window.BidOrBoo.getJobImgUploaderWidget = window.BidOrBoo.getJobImgUploaderWidget
-    ? window.BidOrBoo.getJobImgUploaderWidget
-    : (
-        onSuccessHandler = () => null,
-        onCloseHandler = () => null,
-        jobId = `${Math.random()}`,
-        jobIndex = 0,
-      ) => {
-        const userId = resp.data._id;
-        const cloudName = `${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}`;
-        const uploadPreset = `${process.env.REACT_APP_CLOUDINARY_PRESET}`;
-        const apiKey = `${process.env.REACT_APP_CLOUDINARY_KEY}`;
-        if (!window.BidOrBoo.getJobImgUploaderWidget.widget) {
-          window.BidOrBoo.getJobImgUploaderWidget.widget = window.cloudinary.createUploadWidget(
-            {
-              uploadSignature: (callback, paramsToSign) => {
-                axios
-                  .get(ROUTES.API.UTILS.GET.signCloudinaryRequest, {
-                    params: paramsToSign,
-                  })
-                  .then((res) => {
-                    if (res && res.data) {
-                      const { signature } = res.data;
-                      callback(signature);
-                    }
-                  })
-                  .catch((e) => {});
-              },
-              cloudName,
-              uploadPreset,
-              apiKey,
-              sources: ['local'],
-              multiple: false,
-              folder: `${userId}/Jobs/${jobId}`,
-              tag: `${userId}-Jobs`,
-              publicId: `${jobId}-${jobIndex}`,
-              resourceType: 'image',
-              cropping: true,
-              multiple: false,
-              clientAllowedFormats: ['png', 'gif', 'jpeg', 'tiff', 'jpg', 'bmp'],
-              maxFileSize: 3000000, //3MB //3MB
-              maxImageWidth: 600,
-              maxImageHeight: 500,
-              validateMaxWidthHeight: true,
-              minImageWidth: 100,
-              minImageHeight: 100,
-              croppingValidateDimensions: true,
-              croppingShowDimensions: true,
-              croppingShowBackButton: true,
-              showPoweredBy: false,
-              theme: 'minimal',
-              buttonClass: 'button is-primary is-large',
-              buttonCaption: 'Upload image',
-              styles: {
-                palette: {
-                  window: '#FFF',
-                  windowBorder: '#90A0B3',
-                  tabIcon: '#0E2F5A',
-                  menuIcons: '#5A616A',
-                  textDark: '#000000',
-                  textLight: '#FFFFFF',
-                  link: '#0078FF',
-                  action: '#FF620C',
-                  inactiveTabIcon: '#0E2F5A',
-                  error: '#F44235',
-                  inProgress: '#0078FF',
-                  complete: '#20B832',
-                  sourceBg: '#eeeeee',
-                },
-                fonts: {
-                  Roboto: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
-                },
-              },
-              text: {
-                en: {
-                  queue: {
-                    title: 'Upload Queue',
-                    title_uploading_with_counter: 'Uploading {{num}} Assets',
-                    title_uploading: 'Uploading Assets',
-                    mini_title: 'Uploaded',
-                    mini_title_uploading: 'Uploading',
-                    show_completed: 'Show completed',
-                    retry_failed: 'Retry failed',
-                    abort_all: 'Abort all',
-                    upload_more: 'Upload More',
-                    more: 'More',
-                    mini_upload_count: '{{num}} Uploaded',
-                    mini_failed: '{{num}} Failed',
-                    statuses: {
-                      uploading: 'Uploading...',
-                      error: 'Error',
-                      uploaded: 'Done',
-                      aborted: 'Aborted',
-                    },
-                  },
-                  or: 'Or',
-                  close: 'Close',
-                  menu: {
-                    files: 'Add Images',
-                  },
-                  selection_counter: {
-                    selected: 'selected',
-                  },
-                  actions: {
-                    upload: 'Upload',
-                    clear_all: 'Clear all',
-                    log_out: 'Log out',
-                  },
-                  notifications: {
-                    general_error: 'An error has occurred',
-                    general_prompt: 'Are you sure?',
-                    limit_reached: 'No more files can be selected',
-                    invalid_add_url: 'Added URL must be valid',
-                    invalid_public_id: 'Public ID cannot contain \\,?,&,#,%,<,>',
-                    no_new_files: 'File(s) have already been uploaded',
-                  },
-                  landscape_overlay: {
-                    title: "Landscape mode isn't supported",
-                    description: 'Rotate back to portrait mode to continue.',
-                  },
-                  local: {
-                    main_title: 'BidOrBoo upload job pic',
-                  },
-                },
-              },
-            },
-            (error, result) => {
-              if (result && result.event === 'success') {
-                onSuccessHandler(error, result);
-                window.BidOrBoo.getJobImgUploaderWidget().close({ quiet: true });
-              }
-              if (result.event === 'abort') {
-                onCloseHandler();
-                window.BidOrBoo.getJobImgUploaderWidget().close({ quiet: true });
-              }
-            },
-          );
-        }
-        return window.BidOrBoo.getJobImgUploaderWidget.widget;
-      };
-};
+// const initializeJobImgUploaderWidget = (resp) => {
+//   window.BidOrBoo = window.BidOrBoo ? window.BidOrBoo : {};
+//   window.BidOrBoo.getJobImgUploaderWidget = window.BidOrBoo.getJobImgUploaderWidget
+//     ? window.BidOrBoo.getJobImgUploaderWidget
+//     : (
+//         onSuccessHandler = () => null,
+//         onCloseHandler = () => null,
+//         jobId = `${Math.random()}`,
+//         jobIndex = 0,
+//       ) => {
+//         const userId = resp.data._id;
+//         const cloudName = `${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}`;
+//         const uploadPreset = `${process.env.REACT_APP_CLOUDINARY_PRESET}`;
+//         const apiKey = `${process.env.REACT_APP_CLOUDINARY_KEY}`;
+//         if (!window.BidOrBoo.getJobImgUploaderWidget.widget) {
+//           window.BidOrBoo.getJobImgUploaderWidget.widget = window.cloudinary.createUploadWidget(
+//             {
+//               uploadSignature: (callback, paramsToSign) => {
+//                 axios
+//                   .get(ROUTES.API.UTILS.GET.signCloudinaryRequest, {
+//                     params: paramsToSign,
+//                   })
+//                   .then((res) => {
+//                     if (res && res.data) {
+//                       const { signature } = res.data;
+//                       callback(signature);
+//                     }
+//                   })
+//                   .catch((e) => {});
+//               },
+//               cloudName,
+//               uploadPreset,
+//               apiKey,
+//               sources: ['local'],
+//               multiple: false,
+//               folder: `${userId}/Jobs/${jobId}`,
+//               tag: `${userId}-Jobs`,
+//               publicId: `${jobId}-${jobIndex}`,
+//               resourceType: 'image',
+//               cropping: true,
+//               multiple: false,
+//               clientAllowedFormats: ['png', 'gif', 'jpeg', 'tiff', 'jpg', 'bmp'],
+//               maxFileSize: 3000000, //3MB //3MB
+//               maxImageWidth: 600,
+//               maxImageHeight: 500,
+//               validateMaxWidthHeight: true,
+//               minImageWidth: 100,
+//               minImageHeight: 100,
+//               croppingValidateDimensions: true,
+//               croppingShowDimensions: true,
+//               croppingShowBackButton: true,
+//               showPoweredBy: false,
+//               theme: 'minimal',
+//               buttonClass: 'button is-primary is-large',
+//               buttonCaption: 'Upload image',
+//               styles: {
+//                 palette: {
+//                   window: '#FFF',
+//                   windowBorder: '#90A0B3',
+//                   tabIcon: '#0E2F5A',
+//                   menuIcons: '#5A616A',
+//                   textDark: '#000000',
+//                   textLight: '#FFFFFF',
+//                   link: '#0078FF',
+//                   action: '#FF620C',
+//                   inactiveTabIcon: '#0E2F5A',
+//                   error: '#F44235',
+//                   inProgress: '#0078FF',
+//                   complete: '#20B832',
+//                   sourceBg: '#eeeeee',
+//                 },
+//                 fonts: {
+//                   Roboto: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
+//                 },
+//               },
+//               text: {
+//                 en: {
+//                   queue: {
+//                     title: 'Upload Queue',
+//                     title_uploading_with_counter: 'Uploading {{num}} Assets',
+//                     title_uploading: 'Uploading Assets',
+//                     mini_title: 'Uploaded',
+//                     mini_title_uploading: 'Uploading',
+//                     show_completed: 'Show completed',
+//                     retry_failed: 'Retry failed',
+//                     abort_all: 'Abort all',
+//                     upload_more: 'Upload More',
+//                     more: 'More',
+//                     mini_upload_count: '{{num}} Uploaded',
+//                     mini_failed: '{{num}} Failed',
+//                     statuses: {
+//                       uploading: 'Uploading...',
+//                       error: 'Error',
+//                       uploaded: 'Done',
+//                       aborted: 'Aborted',
+//                     },
+//                   },
+//                   or: 'Or',
+//                   close: 'Close',
+//                   menu: {
+//                     files: 'Add Images',
+//                   },
+//                   selection_counter: {
+//                     selected: 'selected',
+//                   },
+//                   actions: {
+//                     upload: 'Upload',
+//                     clear_all: 'Clear all',
+//                     log_out: 'Log out',
+//                   },
+//                   notifications: {
+//                     general_error: 'An error has occurred',
+//                     general_prompt: 'Are you sure?',
+//                     limit_reached: 'No more files can be selected',
+//                     invalid_add_url: 'Added URL must be valid',
+//                     invalid_public_id: 'Public ID cannot contain \\,?,&,#,%,<,>',
+//                     no_new_files: 'File(s) have already been uploaded',
+//                   },
+//                   landscape_overlay: {
+//                     title: "Landscape mode isn't supported",
+//                     description: 'Rotate back to portrait mode to continue.',
+//                   },
+//                   local: {
+//                     main_title: 'BidOrBoo upload job pic',
+//                   },
+//                 },
+//               },
+//             },
+//             (error, result) => {
+//               if (result && result.event === 'success') {
+//                 onSuccessHandler(error, result);
+//                 window.BidOrBoo.getJobImgUploaderWidget().close({ quiet: true });
+//               }
+//               if (result.event === 'abort') {
+//                 onCloseHandler();
+//                 window.BidOrBoo.getJobImgUploaderWidget().close({ quiet: true });
+//               }
+//             },
+//           );
+//         }
+//         return window.BidOrBoo.getJobImgUploaderWidget.widget;
+//       };
+// };
