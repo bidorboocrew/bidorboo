014fbd94ca3f2f209e18c82f708d48dd137bc4e6
saidmadi <bidorboocrew@gmailcom>
2018-11-22 03:19:25 -0500

    clean up

--- client/src/components/FileUploaderComponent.jsx
+++ client/src/components/FileUploaderComponent.jsx
@@ -33,9 +33,11 @@ class MyForm extends React.Component {
   }
 
   componentWillUnmount() {
-    const { acceptedFile } = this.state;
+    const { acceptedFile,croppedFile } = this.state;
     // clean up memory
     acceptedFile ? window.URL.revokeObjectURL(acceptedFile.preview) : null;
+    croppedFile ? window.URL.revokeObjectURL(croppedFile) : null;
+
   }
 
   onDrophandler(files) {
@@ -61,31 +63,35 @@ class MyForm extends React.Component {
   }
 
   dataURItoBlob(dataURI) {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataURI.split(',')[1]);
+    try {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(',')[1]);
 
-    // separate out the mime component
-    var mimeString = dataURI
-      .split(',')[0]
-      .split(':')[1]
-      .split(';')[0];
+      // separate out the mime component
+      var mimeString = dataURI
+        .split(',')[0]
+        .split(':')[1]
+        .split(';')[0];
 
-    // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length);
-    var ia = new Uint8Array(ab);
-    for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+      var ia = new Uint8Array(ab);
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      //Old Code
+      //write the ArrayBuffer to a blob, and you're done
+      //var bb = new BlobBuilder();
+      //bb.append(ab);
+      //return bb.getBlob(mimeString);
+
+      //New Code
+      return new Blob([ab], { type: mimeString });
+    } catch (e) {
+      console.error(e);
     }
-
-    //Old Code
-    //write the ArrayBuffer to a blob, and you're done
-    //var bb = new BlobBuilder();
-    //bb.append(ab);
-    //return bb.getBlob(mimeString);
-
-    //New Code
-    return new Blob([ab], { type: mimeString });
   }
 
   saveCrop(values) {
