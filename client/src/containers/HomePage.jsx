9b44ef27e087a382a676a30d1e8919ea53f24721
saidmadi <bidorboocrew@gmailcom>
2018-12-27 16:29:45 -0500

    task templates

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import * as ROUTES from '../constants/frontend-route-consts';
 import { switchRoute } from '../utils';
+
+import { showLoginDialog } from '../app-state/actions/uiActions';
+import ServiceTemplates from './proposer-flow/components/ServiceTemplates';
+
 import bidsImg from '../assets/images/bids.png';
 import requestImg from '../assets/images/jobs.png';
 
-export default class HomePage extends React.Component {
+class HomePage extends React.Component {
   render() {
+    const { a_showLoginDialog, isLoggedIn } = this.props;
+
     return (
       <div>
         <section className="hero has-text-centered is-small is-dark">
@@ -44,6 +52,19 @@ export default class HomePage extends React.Component {
             </div>
           </div>
         </section>
+        <section className="section">
+          <div className="container">
+            <div class="tabs is-mobile is-centered">
+              <ul>
+                <li>
+                  <a>Task Templates</a>
+                </li>
+              </ul>
+            </div>
+
+            <ServiceTemplates showLoginDialog={a_showLoginDialog} isLoggedIn={isLoggedIn} />
+          </div>
+        </section>
       </div>
     );
   }
@@ -64,3 +85,19 @@ const BidOrBooCard = (props) => {
     </div>
   );
 };
+
+const mapStateToProps = ({ userReducer }) => {
+  return {
+    isLoggedIn: userReducer.isLoggedIn,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    a_showLoginDialog: bindActionCreators(showLoginDialog, dispatch),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(HomePage);
