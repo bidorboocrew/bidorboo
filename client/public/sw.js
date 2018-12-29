f5f8cfb9459b1ede62f79f0857318e31e8a312c7
Said Madi <35621639+bidorboocrew@users.noreply.github.com>
2018-12-29 00:15:18 -0500

    Update sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -8,12 +8,8 @@ self.addEventListener('push', (event) => {
   const title = data.title;
   const options = {
     body: data.body,
-    icon:
-      'https://res-console.cloudinary.com/hr6bwgs1p/thumbnails/v1/image/upload/v1545981752/QmlkT3JCb28vYW5kcm9pZC1jaHJvbWUtMTkyeDE5Mg==/grid',
-
-    badge:
-      'https://res-console.cloudinary.com/hr6bwgs1p/thumbnails/v1/image/upload/v1545981752/QmlkT3JCb28vYW5kcm9pZC1jaHJvbWUtMTkyeDE5Mg==/grid',
-
+    icon: '/android-chrome-192x192.png',
+    badge: '/android-chrome-192x192.png',
 
     data: data.urlToLaunch || 'https://www.bidorboo.com',
   };
