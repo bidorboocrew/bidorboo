54edc96d5594a60d40d222853d24cd2e0a931efe
saidmadi <bidorboocrew@gmailcom>
2019-02-09 18:23:45 -0500

    fixes

--- client/src/containers/Header.jsx
+++ client/src/containers/Header.jsx
@@ -470,9 +470,16 @@ class Header extends React.Component {
                           <a onClick={this.toggleProfileMenu} className="navbar-link">
                             <figure className="image is-32x32">
                               <img
-                                style={{ paddingRight: 4 }}
+                                style={{
+                                  objectFit: 'cover !important',
+                                  width: '100% !important',
+                                  height: '8rem !important',
+                                  paddingRight: 4,
+                                  borderRadius: '100%',
+                                  height: 'auto',
+                                  width: 32,
+                                }}
                                 src={profileImage.url}
-                                alt="BidOrBoo"
                               />
                             </figure>
                             {displayName}
