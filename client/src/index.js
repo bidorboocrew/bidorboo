2bd961a6064fe2283479fc3ec9734f867392aec9
Said Madi <saidymadi@gmail.com>
2019-04-14 22:40:06 -0400

    remove bugsnag in dev

--- client/src/index.js
+++ client/src/index.js
@@ -24,9 +24,23 @@ window.BidorBoo.stripe = Object.freeze(stripe);
 const bugsnagClient = bugsnag(`${process.env.REACT_APP_BUGSNAG_SECRET}`);
 bugsnagClient.use(bugsnagReact, React);
 const ErrorBoundary = bugsnagClient.getPlugin('react');
-
-ReactDOM.render(
-  <ErrorBoundary>
+if (process.env.NODE_ENV === 'production') {
+  ReactDOM.render(
+    <ErrorBoundary>
+      <StripeProvider apiKey={`${process.env.REACT_APP_STRIPE_KEY}`}>
+        <Provider store={store}>
+          <Router history={appHistory}>
+            <GetNotificationsAndScroll>
+              <App />
+            </GetNotificationsAndScroll>
+          </Router>
+        </Provider>
+      </StripeProvider>
+    </ErrorBoundary>,
+    document.getElementById('BidOrBoo-app'),
+  );
+} else {
+  ReactDOM.render(
     <StripeProvider apiKey={`${process.env.REACT_APP_STRIPE_KEY}`}>
       <Provider store={store}>
         <Router history={appHistory}>
@@ -35,7 +49,7 @@ ReactDOM.render(
           </GetNotificationsAndScroll>
         </Router>
       </Provider>
-    </StripeProvider>
-  </ErrorBoundary>,
-  document.getElementById('BidOrBoo-app'),
-);
+    </StripeProvider>,
+    document.getElementById('BidOrBoo-app'),
+  );
+}
