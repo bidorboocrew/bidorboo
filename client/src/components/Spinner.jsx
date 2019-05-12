2515a5ae81674676d51ad5ccd7671df6fb728b9f
Said Madi <saidymadi@gmail.com>
2019-05-12 19:37:00 -0400

    spinner fixes

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import Delay from 'react-delay';
+import classNames from 'classnames';
 
 export const Spinner = (props) => {
-  return <Delay wait={800}>{props.isLoading && <div id="loading" />}</Delay>;
+  const spinnerSize = classNames(
+    'bdb-spinner',
+    { small: props.size === 'small' },
+    { meduim: props.size === 'meduim' },
+    { large: props.size === 'large' }
+  );
+  return <Delay wait={800}>{props.isLoading ? <div className={spinnerSize} /> : null}</Delay>;
 };
