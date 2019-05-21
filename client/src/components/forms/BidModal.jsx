215fb52c3251adee9435a87ac112c3d5932650cb
Said Madi <saidymadi@gmail.com>
2019-05-21 01:22:28 -0400

    reduce recaptcha issues

---
+++ client/src/components/forms/BidModal.jsx
@@ -0,0 +1,243 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
+import { TextInput } from './FormsHelpers';
+import { enforceNumericField } from './FormsValidators';
+
+class BidModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.recaptchaRef = React.createRef();
+  }
+
+  onAutoBid = (val) => {
+    const { setFieldValue } = this.props;
+    setFieldValue('bidAmountField', val, true);
+  };
+
+  componentDidUpdate() {
+    const { values } = this.props;
+    if (
+      this.recaptchaRef &&
+      this.recaptchaRef.current &&
+      this.recaptchaRef.current.execute &&
+      !values.recaptchaField
+    ) {
+      this.recaptchaRef.current.execute();
+    }
+  }
+  componentWillUnmount() {
+    const { resetForm, setFieldValue, handleClose } = this.props;
+    setFieldValue('bidAmountField', '', false);
+    setFieldValue('recaptchaField', '', false);
+    resetForm();
+    console.log('unmounting');
+  }
+
+  render() {
+    const {
+      values,
+      touched,
+      errors,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleClose,
+      isValid,
+      isSubmitting,
+      avgBid,
+      setFieldValue,
+    } = this.props;
+
+    const autoBidOptions =
+      avgBid < 10 ? (
+        <div className="buttons">
+          <span
+            onClick={() => this.onAutoBid(25)}
+            className="button is-success is-outlined is-small"
+          >
+            25$
+          </span>
+          <span
+            onClick={() => this.onAutoBid(50)}
+            className="button is-success is-outlined is-small"
+          >
+            50$
+          </span>
+          <span
+            onClick={() => this.onAutoBid(100)}
+            className="button is-success is-outlined is-small"
+          >
+            100$
+          </span>
+          <span
+            onClick={() => this.onAutoBid(125)}
+            className="button is-success is-outlined is-small"
+          >
+            125$
+          </span>
+          <span
+            onClick={() => this.onAutoBid(150)}
+            className="button is-success is-outlined is-small"
+          >
+            150$
+          </span>
+        </div>
+      ) : (
+        <div className="buttons">
+          <span style={{ marginRight: 6 }} className="has-text-grey">{`Smart Bid `}</span>
+          <span
+            onClick={() => this.onAutoBid(avgBid - 10)}
+            className="button is-success is-outlined is-small"
+          >
+            {`${avgBid - 10}$`}
+          </span>
+          <span
+            onClick={() => this.onAutoBid(avgBid - 5)}
+            className="button is-success is-outlined is-small"
+          >
+            {`${avgBid - 5}$`}
+          </span>
+          <span
+            onClick={() => this.onAutoBid(avgBid)}
+            className="button is-success is-outlined is-small"
+          >
+            {`${avgBid}$`}
+          </span>
+          <span
+            onClick={() => this.onAutoBid(avgBid + 5)}
+            className="button is-success is-outlined is-small"
+          >
+            {`${avgBid + 5}$`}
+          </span>
+          <span
+            onClick={() => this.onAutoBid(avgBid + 10)}
+            className="button is-success is-outlined is-small"
+          >
+            {`${avgBid + 10}$`}
+          </span>
+        </div>
+      );
+    return (
+      <div className="modal is-active">
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div className="modal-card-title">Place Your Bid!</div>
+            <button onClick={handleClose} className="delete" aria-label="close" />
+          </header>
+          <section className="modal-card-body">
+            <p>
+              Based on the request details that you've read, please enter the total payment amount
+              you'd like to recieve in exchange for doing this task.
+            </p>
+            <br />
+            <input
+              id="recaptchaField"
+              className="input is-invisible"
+              type="hidden"
+              value={values.recaptchaField || ''}
+            />
+            <ReCAPTCHA
+              style={{ display: 'none' }}
+              onExpired={() => this.recaptchaRef.current.execute()}
+              ref={this.recaptchaRef}
+              size="invisible"
+              badge="bottomright"
+              onChange={(result) => {
+                setFieldValue('recaptchaField', result, true);
+                console.log('we got it recaptcha');
+              }}
+              sitekey={`${process.env.REACT_APP_RECAPTCHA_KEY}`}
+            />
+            <TextInput
+              // setFocusImmediately={true}
+              label="Your Bid"
+              id="bidAmountField"
+              className="input is-focused"
+              type="number"
+              onBlur={handleBlur}
+              // helpText={
+              //   avgBid > 0
+              //     ? `*Current Avg bid is $ ${avgBid}$ (CAD)`
+              //     : `* Bid Amount are in (CAD). E.g 50`
+              // }
+              error={touched.bidAmountField && errors.bidAmountField}
+              value={values.bidAmountField || ''}
+              onChange={(e) => {
+                const normalizedVal = enforceNumericField(e.target.value);
+                e.target.value = normalizedVal;
+                handleChange(e);
+              }}
+            />
+            <div style={{ marginTop: -8 }}>
+              <div className="help">* Use our quick bid options</div>
+              {autoBidOptions}
+            </div>
+            <br />
+            <div className="field">
+              <div className="label">BidOrBoo Rules</div>
+              <div className="help">
+                * After you submit. The bid will be reviewed by the requester
+              </div>
+              <div className="help">
+                * You will be assigned to this task if the requester selects you
+              </div>
+              <div className="help">
+                {`* `}
+                <strong>
+                  Canceling after being assigned will negatively impact your rating or if done
+                  frequently will put a ban on your account
+                </strong>
+              </div>
+            </div>
+          </section>
+
+          <footer className="modal-card-foot">
+            <button
+              type="submit"
+              disabled={isSubmitting || !isValid}
+              onClick={handleSubmit}
+              className="button is-success"
+            >
+              Submit Bid
+            </button>
+            <button onClick={handleClose} className="button is-outline">
+              Cancel
+            </button>
+          </footer>
+        </div>
+      </div>
+    );
+  }
+}
+
+const EnhancedForms = withFormik({
+  validationSchema: Yup.object().shape({
+    bidAmountField: Yup.number()
+      .positive('Can only have positive integers')
+      .max(9999, 'The maximum amout is 9999')
+      .required('amount is required.'),
+    recaptchaField: Yup.string()
+      .min(1)
+      .notRequired(
+        'This client failed recaptcha secure check. please refresh the page or try again later.',
+      ),
+  }),
+  mapPropsToValues: (props) => {
+    return {
+      recaptchaField: '',
+    };
+  },
+  handleSubmit: (values, { setSubmitting, props }) => {
+    props.onSubmit(values);
+    setSubmitting(false);
+  },
+
+  displayName: 'BidOnJobForm',
+});
+
+export default EnhancedForms(BidModal);
--- client/src/components/forms/PostYourBid.jsx
+++ client/src/components/forms/PostYourBid.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReCAPTCHA from 'react-google-recaptcha';
 
-import { withFormik } from 'formik';
-import PropTypes from 'prop-types';
-import * as Yup from 'yup';
-import { TextInput } from './FormsHelpers';
-import { enforceNumericField } from './FormsValidators';
+import BidModal from './BidModal';
 
-class PostYourBid extends React.Component {
+export default class PostYourBid extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       showBidDialog: false,
     };
-    this.recaptchaRef = React.createRef();
   }
   closeShowBidDialog = () => {
-    const { resetForm, setFieldValue } = this.props;
-    setFieldValue('bidAmountField', '', false);
-    setFieldValue('recaptchaField', '', false);
-    resetForm();
-
     this.setState({ showBidDialog: false });
   };
 
@@ -30,108 +18,8 @@ class PostYourBid extends React.Component {
     this.setState({ showBidDialog: true });
   };
 
-  onAutoBid = (val) => {
-    const { setFieldValue } = this.props;
-    setFieldValue('bidAmountField', val, true);
-  };
-
-  componentDidUpdate() {
-    const { values } = this.props;
-    if (
-      this.recaptchaRef &&
-      this.recaptchaRef.current &&
-      this.recaptchaRef.current.execute &&
-      !values.recaptchaField
-    ) {
-      this.recaptchaRef.current.execute();
-    }
-  }
-
   render() {
-    const {
-      values,
-      touched,
-      errors,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-
-      isValid,
-      isSubmitting,
-      avgBid,
-      setFieldValue,
-    } = this.props;
     const { showBidDialog } = this.state;
-
-    const autoBidOptions =
-      avgBid < 10 ? (
-        <div className="buttons">
-          <span
-            onClick={() => this.onAutoBid(25)}
-            className="button is-success is-outlined is-small"
-          >
-            25$
-          </span>
-          <span
-            onClick={() => this.onAutoBid(50)}
-            className="button is-success is-outlined is-small"
-          >
-            50$
-          </span>
-          <span
-            onClick={() => this.onAutoBid(100)}
-            className="button is-success is-outlined is-small"
-          >
-            100$
-          </span>
-          <span
-            onClick={() => this.onAutoBid(125)}
-            className="button is-success is-outlined is-small"
-          >
-            125$
-          </span>
-          <span
-            onClick={() => this.onAutoBid(150)}
-            className="button is-success is-outlined is-small"
-          >
-            150$
-          </span>
-        </div>
-      ) : (
-        <div className="buttons">
-          <span style={{ marginRight: 6 }} className="has-text-grey">{`Smart Bid `}</span>
-          <span
-            onClick={() => this.onAutoBid(avgBid - 10)}
-            className="button is-success is-outlined is-small"
-          >
-            {`${avgBid - 10}$`}
-          </span>
-          <span
-            onClick={() => this.onAutoBid(avgBid - 5)}
-            className="button is-success is-outlined is-small"
-          >
-            {`${avgBid - 5}$`}
-          </span>
-          <span
-            onClick={() => this.onAutoBid(avgBid)}
-            className="button is-success is-outlined is-small"
-          >
-            {`${avgBid}$`}
-          </span>
-          <span
-            onClick={() => this.onAutoBid(avgBid + 5)}
-            className="button is-success is-outlined is-small"
-          >
-            {`${avgBid + 5}$`}
-          </span>
-          <span
-            onClick={() => this.onAutoBid(avgBid + 10)}
-            className="button is-success is-outlined is-small"
-          >
-            {`${avgBid + 10}$`}
-          </span>
-        </div>
-      );
     return (
       <React.Fragment>
         <a
@@ -145,126 +33,8 @@ class PostYourBid extends React.Component {
           <span>Place Your Bid</span>
         </a>
 
-        {showBidDialog &&
-          ReactDOM.createPortal(
-            <div className="modal is-active">
-              <div className="modal-background" />
-              <div className="modal-card">
-                <header className="modal-card-head">
-                  <div className="modal-card-title">Place Your Bid!</div>
-                  <button onClick={this.closeShowBidDialog} className="delete" aria-label="close" />
-                </header>
-                <section className="modal-card-body">
-                  <p>
-                    Based on the request details that you've read, please enter the total payment
-                    amount you'd like to recieve in exchange for doing this task.
-                  </p>
-                  <br />
-                  <input
-                    id="recaptchaField"
-                    className="input is-invisible"
-                    type="hidden"
-                    value={values.recaptchaField || ''}
-                  />
-                  <ReCAPTCHA
-                    style={{ display: 'none' }}
-                    onExpired={() => this.recaptchaRef.current.execute()}
-                    ref={this.recaptchaRef}
-                    size="invisible"
-                    badge="bottomright"
-                    onChange={(result) => {
-                      setFieldValue('recaptchaField', result, true);
-                    }}
-                    sitekey={`${process.env.REACT_APP_RECAPTCHA_KEY}`}
-                  />
-                  <TextInput
-                    // setFocusImmediately={true}
-                    label="Your Bid"
-                    id="bidAmountField"
-                    className="input is-focused"
-                    type="number"
-                    onBlur={handleBlur}
-                    // helpText={
-                    //   avgBid > 0
-                    //     ? `*Current Avg bid is $ ${avgBid}$ (CAD)`
-                    //     : `* Bid Amount are in (CAD). E.g 50`
-                    // }
-                    error={touched.bidAmountField && errors.bidAmountField}
-                    value={values.bidAmountField || ''}
-                    onChange={(e) => {
-                      const normalizedVal = enforceNumericField(e.target.value);
-                      e.target.value = normalizedVal;
-                      handleChange(e);
-                    }}
-                  />
-                  <div style={{ marginTop: -8 }}>
-                    <div className="help">* Use our quick bid options</div>
-                    {autoBidOptions}
-                  </div>
-                  <br />
-                  <div className="field">
-                    <div className="label">BidOrBoo Rules</div>
-                    <div className="help">
-                      * After you submit. The bid will be reviewed by the requester
-                    </div>
-                    <div className="help">
-                      * You will be assigned to this task if the requester selects you
-                    </div>
-                    <div className="help">
-                      {`* `}
-                      <strong>
-                        Canceling after being assigned will negatively impact your rating or if done
-                        frequently will put a ban on your account
-                      </strong>
-                    </div>
-                  </div>
-                </section>
-
-                <footer className="modal-card-foot">
-                  <button
-                    type="submit"
-                    disabled={isSubmitting || !isValid}
-                    onClick={handleSubmit}
-                    className="button is-success"
-                  >
-                    Submit Bid
-                  </button>
-                  <button onClick={this.closeShowBidDialog} className="button is-outline">
-                    Cancel
-                  </button>
-                </footer>
-              </div>
-            </div>,
-            document.querySelector('#bidorboo-root-modals'),
-          )}
+        {showBidDialog && <BidModal {...this.props} handleClose={this.closeShowBidDialog} />}
       </React.Fragment>
     );
   }
 }
-
-const EnhancedForms = withFormik({
-  validationSchema: Yup.object().shape({
-    bidAmountField: Yup.number()
-      .positive('Can only have positive integers')
-      .max(9999, 'The maximum amout is 9999')
-      .required('amount is required.'),
-    recaptchaField: Yup.string()
-      .min(1)
-      .notRequired(
-        'This client failed recaptcha secure check. please refresh the page or try again later.',
-      ),
-  }),
-  mapPropsToValues: (props) => {
-    return {
-      recaptchaField: '',
-    };
-  },
-  handleSubmit: (values, { setSubmitting, props }) => {
-    props.onSubmit(values);
-    setSubmitting(false);
-  },
-
-  displayName: 'BidOnJobForm',
-});
-
-export default EnhancedForms(PostYourBid);
