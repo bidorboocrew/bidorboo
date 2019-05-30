2f31665559a7352038d88a47db872f3c5d155635
Said Madi <saidymadi@gmail.com>
2019-05-30 09:55:01 -0400

    fixes to review

--- client/src/utils.js
+++ client/src/utils.js
@@ -23,7 +23,6 @@ export const goBackToPreviousRoute = () => {
 };
 
 export const throwErrorNotification = (dispatch, error) => {
-  debugger
   let msg = error || 'oops ! something went wrong. We apologise for the inconvenience';
   if (error && error.response && error.response.status === 401) {
     let msg = 'You are not authorized! login to perform this action';
@@ -41,7 +40,7 @@ export const throwErrorNotification = (dispatch, error) => {
         ? JSON.stringify(error.response.data)
         : JSON.stringify(error);
   }
-  debugger
+
   dispatch({
     type: A.UI_ACTIONS.SHOW_TOAST_MSG,
     payload: {
