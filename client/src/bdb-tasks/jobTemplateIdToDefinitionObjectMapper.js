cca9153bb3b735ce529b49a6609800f5045be157
Said Madi <saidymadi@gmail.com>
2019-06-12 22:57:54 -0400

    fixes

--- client/src/bdb-tasks/jobTemplateIdToDefinitionObjectMapper.js
+++
@@ -1,8 +0,0 @@
-import TASKS_DEFINITIONS from './index';
-
-// map id to definition object
-const tasksDefinitions = {
-  [`${TASKS_DEFINITIONS.ID}`]: TASKS_DEFINITIONS,
-};
-
-export default tasksDefinitions;
