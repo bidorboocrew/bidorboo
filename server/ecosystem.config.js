571df05ec9cc9a787f40f8087649f9372fa9cd64
Said Madi <saidymadi@gmail.com>
2019-06-24 22:13:25 -0400

    new changes to pm2 eco system

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,14 +3,13 @@ module.exports = {
     {
       name: 'BidOrBoo',
       script: './index.js',
-
+      watch: true,
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       // args: 'one two',
-      instances: 4,
+      instances: 5,
       autorestart: true,
-      max_restarts: 10,
-      watch: false,
-      max_memory_restart: '512M',
+      max_restarts: 5,
+      max_memory_restart: '600M',
       env: {
         NODE_ENV: 'development',
       },
