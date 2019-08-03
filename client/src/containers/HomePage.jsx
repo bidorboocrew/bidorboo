51399d8e1b20d6894a261d49cd403b6ec749204e
Said Madi <saidymadi@gmail.com>
2019-08-03 18:24:06 -0400

    fixes

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 
 import * as ROUTES from '../constants/frontend-route-consts';
 import { switchRoute } from '../utils';
+import howItWorks from '../assets/images/howItWorks.png';
+import MainBanner from '../assets/images/MainBanner.png';
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div>
         <section
-          style={{ background: '#ee2a36', marginBottom: '3rem' }}
-          className="hero has-text-centered is-transparent"
+          style={{
+            background: '#ee2a36',
+            marginBottom: '3.5rem',
+          }}
+          className="hero has-text-centered"
         >
-          <div style={{ position: 'relative' }} className="hero-body">
+          <div
+            style={{ position: 'relative', backgroundImage: `url(${MainBanner})` }}
+            className="hero-body"
+          >
             <div
               style={{
                 position: 'absolute',
@@ -63,14 +71,8 @@ export default class HomePage extends React.Component {
                 }}
                 className="title is-1"
               >
-                <span id="BidOrBoo-welcome-step">
-                  BidOrBoo
-                  {/* <span style={{ color: 'black' }}> Bid</span>
-                  <span style={{ color: 'rgba(0,0,0,0.4)' }}>Or</span>
-                  <span style={{ color: 'black' }}>Boo</span> */}
-                </span>
+                <span id="BidOrBoo-welcome-step">BidOrBoo</span>
               </h1>
-              {/* <Fade> */}
               <h2 className="is-5 has-text-grey-lighter">
                 Get Your Chores Done For The Right Price. Earn Money Doing What You Enjoy.
               </h2>
@@ -103,7 +105,11 @@ export default class HomePage extends React.Component {
             borderBottom: '1px solid #ee2a36',
           }}
         />
-        <div>Video</div>
+        <div className="columns is-mobile is-multiline is-centered">
+          <div style={{ maxWidth: '21rem', marginBottom: '1.5rem' }} className="column">
+            <VideoExplanation />
+          </div>
+        </div>
       </div>
     );
   }
@@ -111,7 +117,7 @@ export default class HomePage extends React.Component {
 
 const RequestAService = (props) => {
   return (
-    <div style={{ height: '30rem' }} className="card cardWithButton">
+    <div style={{ height: '28rem' }} className="card cardWithButton">
       <div className="card-image">
         <figure className="image is-4by3">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
@@ -143,7 +149,7 @@ const RequestAService = (props) => {
 
 const ProvideAService = (props) => {
   return (
-    <div style={{ height: '30rem' }} className="card cardWithButton">
+    <div style={{ height: '28rem' }} className="card cardWithButton">
       <div className="card-image">
         <figure className="image is-4by3">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
@@ -180,13 +186,13 @@ const HowItWorksRequestService = () => {
       </h1>
       <ul>
         <li>
-          <p className="is-size-6">Fill A Request</p>
+          <p className="is-size-5">Fill A Request</p>
         </li>
         <li>
-          <p className="is-size-6">Taskers Will Bid</p>
+          <p className="is-size-5">Taskers Will Bid</p>
         </li>
         <li>
-          <p className="is-size-6">Chose a Tasker</p>
+          <p className="is-size-5">Chose a Tasker</p>
         </li>
       </ul>
     </div>
@@ -201,15 +207,34 @@ const HowItWorksProvideService = () => {
       </h1>
       <ul>
         <li>
-          <p className="is-size-6">Browse Tasks</p>
+          <p className="is-size-5">Browse Tasks</p>
         </li>
         <li>
-          <p className="is-size-6">Place Your Bids</p>
+          <p className="is-size-5">Place Your Bids</p>
         </li>
         <li>
-          <p className="is-size-6">{`Do it & Get Paid.`}</p>
+          <p className="is-size-5">{`Do it & Get Paid.`}</p>
         </li>
       </ul>
+    </div>
+  );
+};
+
+const VideoExplanation = () => {
+  return (
+    <div style={{ height: 'unset' }} className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={howItWorks} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="content has-text-centered">
+          <div style={{ marginBottom: 0, color: '#ee2a36' }} className="title is-size-4">
+            See How It Works
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
