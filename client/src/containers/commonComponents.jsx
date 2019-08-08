2c79838b38fa538b68eab74c558a784ce734080c
Said Madi <saidymadi@gmail.com>
2019-08-08 17:23:42 -0400

    remove unusedcode

--- client/src/containers/commonComponents.jsx
+++ client/src/containers/commonComponents.jsx
@@ -202,57 +202,57 @@ export const CenteredUserImageAndRating = ({
       </div>
     </div>
   );
-  return (
-    <div
-      style={{
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        margin: isCentered ? 'auto' : '',
-        width: '13rem',
-        overflow: 'hidden',
-      }}
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        switchRoute(ROUTES.CLIENT.dynamicUserProfileForReview(userDetails._id));
-      }}
-    >
-      <figure style={{ margin: '0 8px 0 0' }} className="media-left">
-        <img
-          className={`image ${large ? 'is-64x64' : 'is-48x48'} `}
-          style={{
-            borderRadius: '100%',
-            width: `${large ? 64 : 48}`,
-            height: `${large ? 64 : 48}`,
-            boxShadow: '0 2px 8px 0 rgba(0, 0, 0, 0.34)',
-          }}
-          src={profileImage.url}
-          alt="image"
-        />
-      </figure>
+  // return (
+  //   <div
+  //     style={{
+  //       cursor: 'pointer',
+  //       display: 'flex',
+  //       alignItems: 'center',
+  //       margin: isCentered ? 'auto' : '',
+  //       width: '13rem',
+  //       overflow: 'hidden',
+  //     }}
+  //     onClick={(e) => {
+  //       e.preventDefault();
+  //       e.stopPropagation();
+  //       switchRoute(ROUTES.CLIENT.dynamicUserProfileForReview(userDetails._id));
+  //     }}
+  //   >
+  //     <figure style={{ margin: '0 8px 0 0' }} className="media-left">
+  //       <img
+  //         className={`image ${large ? 'is-64x64' : 'is-48x48'} `}
+  //         style={{
+  //           borderRadius: '100%',
+  //           width: `${large ? 64 : 48}`,
+  //           height: `${large ? 64 : 48}`,
+  //           boxShadow: '0 2px 8px 0 rgba(0, 0, 0, 0.34)',
+  //         }}
+  //         src={profileImage.url}
+  //         alt="image"
+  //       />
+  //     </figure>
 
-      <div className="content">
-        <div className={`${large ? 'is-size-5' : 'is-size-6'}`}>{trimmedDisplayName}</div>
+  //     <div className="content">
+  //       <div className={`${large ? 'is-size-5' : 'is-size-6'}`}>{trimmedDisplayName}</div>
 
-        {rating.globalRating === 'No Ratings Yet' || rating.globalRating === 0 ? (
-          <div className="has-text-grey" style={{ lineHeight: '52px', fontSize: 18 }}>
-            <span className="icon">
-              <i className="far fa-star" />
-            </span>
-            <span>Not Rated</span>
-          </div>
-        ) : (
-          <div className="has-text-dark" style={{ lineHeight: '52px', fontSize: 18 }}>
-            <span className="icon">
-              <i className="fas fa-star" />
-            </span>
-            <span>{rating.globalRating}</span>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+  //       {rating.globalRating === 'No Ratings Yet' || rating.globalRating === 0 ? (
+  //         <div className="has-text-grey" style={{ lineHeight: '52px', fontSize: 18 }}>
+  //           <span className="icon">
+  //             <i className="far fa-star" />
+  //           </span>
+  //           <span>Not Rated</span>
+  //         </div>
+  //       ) : (
+  //         <div className="has-text-dark" style={{ lineHeight: '52px', fontSize: 18 }}>
+  //           <span className="icon">
+  //             <i className="fas fa-star" />
+  //           </span>
+  //           <span>{rating.globalRating}</span>
+  //         </div>
+  //       )}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export const CardTitleAndActionsInfo = ({
