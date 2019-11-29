4f754134382215b5828a755049452c509561c8f3
Said Madi <saidymadi@gmail.com>
2019-11-29 12:38:39 -0500

    fixes

--- server/middleware/requireBidorBooHost.js
+++ server/middleware/requireBidorBooHost.js
@@ -3,7 +3,7 @@ const keys = require('../config/keys');
 module.exports = (req, res, next) => {
   if (process.env.NODE_ENV !== 'production') {
     next();
-  } else if (keys.allowedHostNames.indexOf(req.hostname) > -1) {
+  } else if (keys.allowedHostName.indexOf(req.hostname) > -1) {
     return res.status(401).send('Navigate to our official secure site  https://www.bidorboo.ca');
   } else {
     next();
