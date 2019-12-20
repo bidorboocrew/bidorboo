f0565f1e85d9b2d1bfd334797c420bf53e724a6b
Said Madi <saidymadi@gmail.com>
2019-12-19 19:22:41 -0500

    fixes

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -5,14 +5,11 @@ export { default as MyProfile } from './personal-profile/MyProfile';
 export { default as PaymentSettings } from './personal-profile/PaymentSettings';
 export { default as NotificationSettings } from './personal-profile/MyNotifications';
 
-
 // requester flow
 export { default as RequesterRootPage } from './requester-flow/RequesterRootPage';
 export { default as CreateARequestPage } from './requester-flow/CreateARequestPage';
 export { default as MyRequestsPage } from './requester-flow/MyRequestsPage';
-export {
-  default as ReviewMyAwardedRequestAndWinningBidPage,
-} from './requester-flow/ReviewMyAwardedRequestAndWinningBidPage';
+export { default as ReviewMyAwardedRequestAndWinningBidPage } from './requester-flow/ReviewMyAwardedRequestAndWinningBidPage';
 export { default as ReviewRequestAndBidsPage } from './requester-flow/ReviewRequestAndBidsPage';
 
 // tasker flow
@@ -22,11 +19,7 @@ export { default as ReviewBidAndRequestPage } from './tasker-flow/ReviewOpenBidA
 export { default as ReviewAwardedBidPage } from './tasker-flow/ReviewAwardedBidPage';
 export { default as MyBidsPage } from './tasker-flow/MyBidsPage';
 
-export { default as NotificationsModal } from './NotificationsModal';
-
-export {
-  default as RequesterReviewingCompletedRequest,
-} from './review-flow/RequesterReviewingCompletedRequest';
+export { default as RequesterReviewingCompletedRequest } from './review-flow/RequesterReviewingCompletedRequest';
 export { default as TaskerReviewingCompletedRequest } from './review-flow/TaskerReviewingCompletedRequest';
 
 export { default as OtherUserProfileForReviewPage } from './OtherUserProfileForReviewPage';
