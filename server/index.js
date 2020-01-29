8eb683a0c6f1012724e5d86c1491da1bc92235e3
Said Madi <saidymadi@gmail.com>
2020-01-28 19:20:29 -0500

    fixes

--- server/index.js
+++ server/index.js
@@ -139,16 +139,18 @@ if (process.env.NODE_ENV === 'production') {
 
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('../client/build'), {
-    setHeaders: (res, path, stat) => {
-      console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+  app.use(
+    express.static('../client/build', {
+      setHeaders: (res, path, stat) => {
+        console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 
-      console.log(path);
-      console.log('----------');
-      console.log(stat);
-      console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxEnd');
-    },
-  });
+        console.log(path);
+        console.log('----------');
+        console.log(stat);
+        console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxEnd');
+      },
+    })
+  );
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
