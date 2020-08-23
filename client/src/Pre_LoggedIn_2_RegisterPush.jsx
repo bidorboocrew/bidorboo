8a3d9f8cd5cd827aeb33fae4c2d0bd7f0d625fc0
Said Madi <saidymadi@gmail.com>
2020-08-22 23:38:11 -0400

    fixes

--- client/src/Pre_LoggedIn_2_RegisterPush.jsx
+++ client/src/Pre_LoggedIn_2_RegisterPush.jsx
@@ -6,6 +6,7 @@ import { getBugsnagClient } from './index';
 import Pre_LoggedIn_3_ScrollUpSetAppUserViewsAndRenderChildren from './Pre_LoggedIn_3_ScrollUpSetAppUserViewsAndRenderChildren';
 
 const updateUserSubscription = (userDetails) => {
+  console.log('updateUserSubscription');
   try {
     if (!window.OneSignal) {
       return;
@@ -13,6 +14,7 @@ const updateUserSubscription = (userDetails) => {
 
     // https://documentation.onesignal.com/docs/web-push-sdk#notificationpermissionchange
     window.OneSignal.push(function () {
+      console.log('window.OneSignal.push updateUserSubscription');
       /* These examples are all valid */
       window.OneSignal.getUserId(function (oneSignalUserId) {
         console.log('OneSignal User ID:', oneSignalUserId);
@@ -29,7 +31,7 @@ const updateUserSubscription = (userDetails) => {
       });
     });
   } catch (e) {
-    console.log('error updateUserSubscription' + e);
+    console.error('error updateUserSubscription' + e);
     getBugsnagClient().leaveBreadcrumb(
       'updateUserSubscription Pre_LoggedIn_3_ScrollUpSetAppUserViewsAndRenderChildren',
     );
@@ -50,77 +52,55 @@ class Pre_LoggedIn_2_RegisterPush extends React.PureComponent {
 
   componentDidMount() {
     const { userDetails } = this.props;
-    console.log({ userDetails });
 
     if (!userDetails.userId) {
-      console.log('!userDetails.userId');
       return;
     }
 
-    console.log({ userDetails });
     updateUserSubscription(userDetails);
     // https://documentation.onesignal.com/docs/sdk-reference
     window.OneSignal.push(function () {
-      const isPushSupported = window.OneSignal.isPushNotificationsSupported();
-      if (!isPushSupported) {
-        return;
-      }
-
       // OneSignal.showNativePrompt();
-      console.log('Push notifications are enabled!');
-      window.OneSignal.getUserId(function (userId) {
-        if (userId === userDetails.userId) {
-          console.log('user already setup');
-          return;
-        }
+      window.OneSignal.setLocationShared && window.OneSignal.setLocationShared(false);
+      window.OneSignal.setDefaultNotificationUrl('https://www.bidorboo.ca');
+      window.OneSignal.setExternalUserId(userDetails.userId);
+      if (userDetails.email && userDetails.email.emailAddress) {
+        window.OneSignal.setEmail(userDetails.email.emailAddress);
+      }
+      window.OneSignal.sendTags({ ...userDetails });
 
-        window.OneSignal.setLocationShared && window.OneSignal.setLocationShared(false);
-        window.OneSignal.setDefaultNotificationUrl('https://www.bidorboo.ca');
-        window.OneSignal.setExternalUserId(userDetails.userId);
-        if (userDetails.email && userDetails.email.emailAddress) {
-          window.OneSignal.setEmail(userDetails.email.emailAddress);
-        }
-        window.OneSignal.sendTags({ ...userDetails });
+      window.OneSignal.on('subscriptionChange', function (isSubscribed) {
+        updateUserSubscription(userDetails);
       });
 
       window.OneSignal.showSlidedownPrompt();
-
-      window.OneSignal.on('subscriptionChange', function (isSubscribed) {
-        updateUserSubscription(userDetails, isSubscribed);
-      });
-
-      window.OneSignal.isPushNotificationsEnabled(function (isEnabled) {
-        if (isEnabled) {
-          // OneSignal.showNativePrompt();
-          console.log('Push notifications are enabled!');
-        } else {
-          console.log('Push notifications are not enabled yet.');
-          window.OneSignal.showSlidedownPrompt();
-        }
-      });
     });
 
-    /**
-     * for android apps only
-     */
-    const androidOneSignalId = window.localStorage.getItem('bob_androidOneSignalPlayerId');
-    if (androidOneSignalId) {
-      if (window.bidorbooAndroid && window.bidorbooAndroid.setExternalUserOneSignalId) {
-        window.bidorbooAndroid.setExternalUserOneSignalId(`${androidOneSignalId}`);
+    try {
+      /**
+       * for android apps only
+       */
+      const androidOneSignalId = window.localStorage.getItem('bob_androidOneSignalPlayerId');
+      if (androidOneSignalId) {
+        if (window.bidorbooAndroid && window.bidorbooAndroid.setExternalUserOneSignalId) {
+          window.bidorbooAndroid.setExternalUserOneSignalId(`${androidOneSignalId}`);
+        }
+        try {
+          // register the user push norification
+          axios.post('/api/push/register', {
+            data: {
+              oneSignalUserId: androidOneSignalId,
+            },
+          });
+        } catch (e) {
+          getBugsnagClient().leaveBreadcrumb(
+            'updateUserSubscription ANDROID Pre_LoggedIn_3_ScrollUpSetAppUserViewsAndRenderChildren',
+          );
+          getBugsnagClient().notify(e);
+        }
       }
-      try {
-        // register the user push norification
-        axios.post('/api/push/register', {
-          data: {
-            oneSignalUserId: androidOneSignalId,
-          },
-        });
-      } catch (e) {
-        getBugsnagClient().leaveBreadcrumb(
-          'updateUserSubscription ANDROID Pre_LoggedIn_3_ScrollUpSetAppUserViewsAndRenderChildren',
-        );
-        getBugsnagClient().notify(e);
-      }
+    } catch (e) {
+      console.error(e);
     }
     /********************android app end************************************* */
   }
